d5cba73696d014133d98ffa42147adfa9bbd0470
Alex Istrate <calzzon3@gmail.com>
2020-03-08 12:21:39 +0200

    updates:

---
+++ github_update.sh
@@ -0,0 +1,9 @@
+git config --global user.name "Alex Istrate"
+git config --global user.email "calzzon3@gmail.com"
+git config --global hub.protocol https 
+
+git pull
+
+sudo chown calzzone:calzzone gcloud_deploy.sh
+sudo chmod +x gcloud_deploy.sh
+
