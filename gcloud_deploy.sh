ca6899d1dd33ee77c72dd05b5ef8996c1909693d
Alex Istrate <calzzon3@gmail.com>
2020-03-08 12:12:31 +0200

    updates:

--- gcloud_deploy.sh
+++ gcloud_deploy.sh
@@ -1,2 +1,1 @@
- hugo -D && sudo rm -R /var/www/html && sudo cp -R ~/behinderrorbars/public /var/www/html
- 
+hugo -D && sudo rm -R /var/www/html && sudo cp -R ~/Behind-Error-Bars/public /var/www/html
